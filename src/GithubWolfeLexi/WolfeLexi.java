1a0c9ffc771a70c13e173acd3b114fac37972468
grimaesthetic <grimaesthetic@users.noreply.github.com>
2020-02-04 11:58:16 -0500

    what is happening right now????????

---
+++ src/GithubWolfeLexi/WolfeLexi.java
@@ -0,0 +1,10 @@
+package GithubWolfeLexi;
+public class WolfeLexi {
+
+    public static void main(String[] args) {
+        Wolfe();
+    }
+    public static void Wolfe(){
+        System.out.println("Lexi Wolfe!!");
+    }
+}
